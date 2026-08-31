56b4366621f2a2063c1db1fc8645bb9410566dad
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-08-30 18:44:24 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
   appName: "Iglesia Fuertes V2",
   storagePrefix: "iglesiaFuertesV2",
-  defaultApiUrl: "https://script.google.com/macros/s/AKfycbwrVAqioeAQY7osip7EMnd-nJZPbd4RB-gzI1sqxHspNXOFzDGOE8_tjyPgWl_wazIX/exec"
+  defaultApiUrl: "https://script.google.com/macros/s/AKfycbyLrwTBmah954d8lI4xR12Pl8-IrIvymSrI0cAP4u11f6J8edMpsat-85nlmI4f053D/exec"
 };
