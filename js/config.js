65a9a0caf442dbba7bc00b565087609670af0723
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-09-02 23:16:21 -0600

    Update default API URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
   appName: "Iglesia Fuertes V2",
   storagePrefix: "iglesiaFuertesV2",
-  defaultApiUrl: "https://script.google.com/macros/s/AKfycbzysCvkaaSPRUEzsYs65_uqSPcR0DTxjU1BK76pAObeEmteGoB2S0kthVW50IHZQCwl/exec"
+  defaultApiUrl: "https://script.google.com/macros/s/AKfycbxEyzmXv7qxCctz2Hg8JfgDfCG95SQcPJ1Z8-_fkKD_ZClBy7HlkHWhaehvtfSIYmGH/exec"
 };
